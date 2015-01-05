48b754448472a2669ca92d4cc3dc2a11c6e36c29
CoryMcCartan <cory.mccartan@gmail.com>
2015-01-05 08:07:55 -0800

    dashboard updates for drivebase current and temp

--- src/com/shsrobotics/recyclerush/RobotDashboard.java
+++ src/com/shsrobotics/recyclerush/RobotDashboard.java
@@ -1,8 +1,18 @@
 package com.shsrobotics.recyclerush;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class RobotDashboard implements Hardware {
 	
 	public static void updateView() {
-		
+		// DRIVE SYSTEM temp and current
+		SmartDashboard.putNumber(Dashboard.FL_TALON_TEMP, Wheels.frontLeft.getTemp());
+		SmartDashboard.putNumber(Dashboard.FL_TALON_CURR, Wheels.frontLeft.getOutputCurrent());
+		SmartDashboard.putNumber(Dashboard.RL_TALON_TEMP, Wheels.rearLeft.getTemp());
+		SmartDashboard.putNumber(Dashboard.RL_TALON_CURR, Wheels.rearLeft.getOutputCurrent());
+		SmartDashboard.putNumber(Dashboard.FR_TALON_TEMP, Wheels.frontRight.getTemp());
+		SmartDashboard.putNumber(Dashboard.FR_TALON_CURR, Wheels.frontRight.getOutputCurrent());
+		SmartDashboard.putNumber(Dashboard.RR_TALON_TEMP, Wheels.rearRight.getTemp());
+		SmartDashboard.putNumber(Dashboard.RR_TALON_CURR, Wheels.rearRight.getOutputCurrent());
 	}
 }
