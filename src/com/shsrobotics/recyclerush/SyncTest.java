bd7f01a30d651a20d1d5b2959207e0f3b6ec09b4
MaxOrth <max.orth96@gmail.com>
2015-01-20 18:45:06 -0800

    Sync Test 3 in progress, part 1

--- src/com/shsrobotics/recyclerush/SyncTest.java
+++ src/com/shsrobotics/recyclerush/SyncTest.java
@@ -13,9 +13,7 @@ public class SyncTest {
 	
 	public static void test() {
 		int a = 0, b = 0, c = 0;
-		System.out.println(a);
-		System.out.println(c-c*a/b+ab);
-		System.out.println(b);
+
 		System.out.println(c);
 	}
 	
