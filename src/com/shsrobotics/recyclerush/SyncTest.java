44365683260e23ddbf6e9f559814ea583a5f6bc4
MaxOrth <max.orth96@gmail.com>
2015-01-20 18:38:53 -0800

    Sync test 2

--- src/com/shsrobotics/recyclerush/SyncTest.java
+++ src/com/shsrobotics/recyclerush/SyncTest.java
@@ -5,5 +5,18 @@ package com.shsrobotics.recyclerush;
  *
  */
 public class SyncTest {
-
+	public SyncTest() {
+		
+	}
+	
+	
+	
+	public static void test() {
+		int a = 0, b = 0, c = 0;
+		System.out.println(a);
+		System.out.println(c-c*a/b+ab);
+		System.out.println(b);
+		System.out.println(c);
+	}
+	
 }
