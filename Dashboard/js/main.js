fb4096ec5a7eb6e281fba144391f31052a111bd5
Cory McCartan <cory.mccartan@gmail.com>
2015-02-21 18:14:11 -0800

    dashboard

--- Dashboard/js/main.js
+++ Dashboard/js/main.js
@@ -1,63 +1,63 @@
 Dashboard = {
-    TABLE_NAME: "ROBOT",
+	TABLE_NAME: "ROBOT",
 //    IP: "RoboRIO-2412.local",
-    IP: "localhost",
-    AUTO_MODE: "auto-mode",
-    PDP_TEMP: "pdp-temp",
-    PDP_VOLT: "pdp-volt",
-    PDP_CURR: "pdp-curr",
-    PDP_POWER: "pdp-power",
-    ELEV_CURR: "elev-curr",
-    GRIPPER_CURR: "gripper-curr",
-    TOTE_COUNT: "toteCount",
-    HAS_RC: "hasRC",
-    X_POSITION: "x-position",
-    Y_POSITION: "y-position",
-    HEADING: "heading",
-    DISABLE_GRIPPER: "disable-gripper",
-    DISABLE_ROLLERS: "disable-rollers",
-    DISABLE_ELEVATOR: "disable-elevator",
-    DISABLE_CLAW: "disable-claw",
-    ELEVATOR_POSITION: "elev-pos",
-    SOFTWARE_VERSION: "version"
+	IP: "localhost",
+	AUTO_MODE: "auto-mode",
+	PDP_TEMP: "pdp-temp",
+	PDP_VOLT: "pdp-volt",
+	PDP_CURR: "pdp-curr",
+	PDP_POWER: "pdp-power",
+	ELEV_CURR: "elev-curr",
+	GRIPPER_CURR: "gripper-curr",
+	TOTE_COUNT: "toteCount",
+	HAS_RC: "hasRC",
+	X_POSITION: "x-position",
+	Y_POSITION: "y-position",
+	HEADING: "heading",
+	DISABLE_GRIPPER: "disable-gripper",
+	DISABLE_ROLLERS: "disable-rollers",
+	DISABLE_ELEVATOR: "disable-elevator",
+	DISABLE_CLAW: "disable-claw",
+	ELEVATOR_POSITION: "elev-pos",
+	SOFTWARE_VERSION: "version"
 };
 
 Autonomous = {
-    ROBOT_SET: 0,
-    TOTE_SET: 1,
-    STACK_SET: 2,
-    CENTER_RC_SET: 3
+	ROBOT_SET: 0,
+	TOTE_SET: 1,
+	STACK_SET: 2,
+	CENTER_RC_SET: 3
 };
 
 Systems = {
-    driveBase: {
-	current: 0,
-	voltage: 0,
-	power: 0,
-	temp: 0
-    },
-    gripper: {
-	current: 0,
-	disabled: false
-    },
-    elevator: {
-	current: 0,
-	position: 0,
-	disabled: false
-    },
-    rollerIntake: {
-	disabled: false
-    },
-    claw: {
-	disabled: false
-    }
+	driveBase: {
+		current: 0,
+		voltage: 0,
+		power: 0,
+		temp: 0
+	},
+	gripper: {
+		current: 0,
+		disabled: false
+	},
+	elevator: {
+		current: 0,
+		position: 0,
+		disabled: false
+	},
+	rollerIntake: {
+		disabled: false
+	},
+	claw: {
+		disabled: false
+	}
 };
 Stack = {
-    totes: 0,
-    hasRC: false,
-    addRC: false,
-    addTote: 0,
-    clear: false
+	totes: 0,
+	hasRC: false,
+	addRC: false,
+	addTote: 0,
+	clear: false
 };
 
 REQ_ANIM_FRAME_ID = -1;
@@ -67,85 +67,93 @@ CONNECTED = false;
 UPDATE_SYSTEM_DISABLE = false;
 UPDATE_AUTONOMOUS = false;
 
-document.addEventListener("readystatechange", function () {
-    if (document.readyState !== "complete")
-	return;
+document.addEventListener("readystatechange", function() {
+	if (document.readyState !== "complete")
+		return;
 
-    N = new NetworkTable(Dashboard.TABLE_NAME, Dashboard.IP);
+	N = new NetworkTable(Dashboard.TABLE_NAME, Dashboard.IP);
 
-    // try to connect until connected
-    var ct = 0;
-    var id = setInterval(function () {
-	if (N.isConnected() || ++ct > 8) {
-	    clearInterval(id);
-	    if (N.isConnected())
-		CONNECTED = true;
-	    init();
-	    REQ_ANIM_FRAME_ID = requestAnimationFrame(update);
-	    GET_VALUES_ID = setInterval(get, 100);
-	} else {
-	    N.connect();
+	// try to connect until connected
+	var ct = 0;
+	var id = setInterval(function() {
+		N.isConnected(function(connected) {
+			CONNECTED = connected;
+			if (connected || ++ct > 8) {
+				clearInterval(id);
+				init();
+				REQ_ANIM_FRAME_ID = requestAnimationFrame(update);
+				GET_VALUES_ID = setInterval(get, 100);
+			} else {
+				N.connect();
+			}
+		});
+	}, 250);
+	REQ_ANIM_FRAME_ID = requestAnimationFrame(update);
+
+	var list = $$("form#autonomousSelection > div");
+	for (var i = 0; i < list.length; i++) {
+		list[i].querySelector("input").onclick = function(e) {
+			e.stopPropagation();
+		};
+		list[i].querySelector("input").onchange = function() {
+			UPDATE_AUTONOMOUS = true;
+		};
+		list[i].onclick = function() {
+			var el = this.querySelector("input");
+			if (!el.checked)
+				UPDATE_AUTONOMOUS = true;
+			el.checked = true;
+		};
 	}
-    }, 250);
-    REQ_ANIM_FRAME_ID = requestAnimationFrame(update);
 
-    var list = $$("form#autonomousSelection > div");
-    for (var i = 0; i < list.length; i++) {
-	list[i].querySelector("input").onclick = function (e) {
-	    e.stopPropagation();
+	list = $$(".statusLight");
+	for (var i = 0; i < list.length; i++) {
+		list[i].ondblclick = function() {
+			switch (this.id) {
+				case "status_ROLLERS":
+					Systems.rollerIntake.disabled = !Systems.rollerIntake.disabled;
+					break;
+				case "status_GRIPPER":
+					Systems.gripper.disabled = !Systems.gripper.disabled;
+					break;
+				case "status_ELEVATOR":
+					Systems.elevator.disabled = !Systems.elevator.disabled;
+					break;
+				case "status_CLAW":
+					Systems.claw.disabled = !Systems.claw.disabled;
+					break;
+			}
+			UPDATE_SYSTEM_DISABLE = true;
+		};
+	}
+	
+	$(".rws").ondblclick = RWS.clear;
+	document.body.onkeypress = function(e) {
+		if (e.keyCode >= 97 && e.keyCode <= 122) { // alphabetic
+			RWS.clear();
+		}
 	};
-	list[i].querySelector("input").onchange = function () {
-	    UPDATE_AUTONOMOUS = true;
-	};
-	list[i].onclick = function () {
-	    var el = this.querySelector("input");
-	    if (!el.checked)
-		UPDATE_AUTONOMOUS = true;
-	    el.checked = true;
-	};
-    }
 
-    list = $$(".statusLight");
-    for (var i = 0; i < list.length; i++) {
-	list[i].onclick = function () {
-	    switch (this.id) {
-		case "status_ROLLERS":
-		    Systems.rollerIntake.disabled = !Systems.rollerIntake.disabled;
-		    break;
-		case "status_GRIPPER":
-		    Systems.gripper.disabled = !Systems.gripper.disabled;
-		    break;
-		case "status_ELEVATOR":
-		    Systems.elevator.disabled = !Systems.elevator.disabled;
-		    break;
-		case "status_CLAW":
-		    Systems.claw.disabled = !Systems.claw.disabled;
-		    break;
-	    }
-	    UPDATE_SYSTEM_DISABLE = true;
+	$("#connect").onclick = function() {
+		N.connect();
+		setTimeout(function() {
+			if (N.isConnected()) {
+				CONNECTED = true;
+				init();
+			}
+		}, 750);
 	};
-    }
-
-    $("#connect").onclick = function () {
-	N.connect();
-	setTimeout(function () {
-	    if (N.isConnected()) {
-		CONNECTED = true;
-		init();
-	    }
-	}, 750);
-    };
-    $("#about").onclick = function () {
-	$("#info").style.display = "block";
-    };
-    $("#info").onclick = function () {
-	$("#info").style.display = "none";
-    };
+	$("#about").onclick = function() {
+		$("#info").style.display = "block";
+	};
+	$("#info").onclick = function() {
+		$("#info").style.display = "none";
+	};
 });
 
-window.$ = function (s) {
-    return document.querySelector(s);
+window.$ = function(s) {
+	return document.querySelector(s);
 };
-window.$$ = function (s) {
-    return document.querySelectorAll(s);
+window.$$ = function(s) {
+	return document.querySelectorAll(s);
 };
--- Dashboard/js/update.js
+++ Dashboard/js/update.js
@@ -2,166 +2,244 @@
  * Get and set numbers from NetworkTable
  */
 function get() {
-    if (!CONNECTED) return;
-    
-    /*
-     * AUTONOMOUS MODE
-     */
-    if (UPDATE_AUTONOMOUS) {
-	var autoMode = Autonomous.ROBOT_SET;
-	if ($("#ROBOT_SET").checked)
-	    autoMode = Autonomous.ROBOT_SET;
-	if ($("#TOTE_SET").checked)
-	    autoMode = Autonomous.TOTE_SET;
-	if ($("#CENTER_RC_SET").checked)
-	    autoMode = Autonomous.CENTER_RC_SET;
-	N.set(Dashboard.AUTO_MODE, autoMode);
-	UPDATE_AUTONOMOUS = false;
-    }
+	if (!CONNECTED)
+		return;
 
-    /*
-     * SYSTEM STATUS
-     */
-    if (UPDATE_SYSTEM_DISABLE) {
-	N.set(Dashboard.DISABLE_ELEVATOR, Systems.elevator.disabled);
-	N.set(Dashboard.DISABLE_GRIPPER, Systems.gripper.disabled);
-	N.set(Dashboard.DISABLE_ROLLERS, Systems.rollerIntake.disabled);
-	N.set(Dashboard.DISABLE_CLAW, Systems.claw.disabled);
-	UPDATE_SYSTEM_DISABLE = false;
-    }
-
-    Systems.driveBase.voltage = N.get(Dashboard.PDP_VOLT, 0.0);
-    Systems.driveBase.current = N.get(Dashboard.PDP_CURR, 0.0);
-    Systems.driveBase.power = N.get(Dashboard.PDP_POWER, 0.0);
-    Systems.driveBase.temp = N.get(Dashboard.PDP_TEMP, 0.0);
-    Systems.elevator.current = N.get(Dashboard.ELEV_CURR, 0.0);
-    Systems.gripper.current = N.get(Dashboard.GRIPPER_CURR, 0.0);
-    Systems.elevator.position = N.get(Dashboard.ELEVATOR_POSITION, 0.0);
-
-    /*
-     * STACK UPDATE
-     */
-    var oldCount = Stack.totes;
-    var oldRC = Stack.hasRC;
-    Stack.totes = N.get(Dashboard.TOTE_COUNT, 0.0);
-    Stack.hasRC = N.get(Dashboard.HAS_RC, false);
-    if (oldRC !== Stack.hasRC) {
-	if (Stack.hasRC) {
-	    Stack.addRC = true;
-	} else { // just released
-	    Stack.clear = true;
+	/*
+	 * AUTONOMOUS MODE
+	 */
+	if (UPDATE_AUTONOMOUS) {
+		var autoMode = Autonomous.ROBOT_SET;
+		if ($("#ROBOT_SET").checked)
+			autoMode = Autonomous.ROBOT_SET;
+		if ($("#TOTE_SET").checked)
+			autoMode = Autonomous.TOTE_SET;
+		if ($("#CENTER_RC_SET").checked)
+			autoMode = Autonomous.CENTER_RC_SET;
+		N.set(Dashboard.AUTO_MODE, autoMode);
+		UPDATE_AUTONOMOUS = false;
 	}
-    }
-    if (oldCount !== Stack.totes) {
-	if (Stack.totes === 0) { // just released
-	    Stack.clear = true;
-	} else {
-	    Stack.addTote = Stack.totes - oldCount;
+
+	/*
+	 * SYSTEM STATUS
+	 */
+	if (UPDATE_SYSTEM_DISABLE) {
+		N.set(Dashboard.DISABLE_ELEVATOR, Systems.elevator.disabled);
+		N.set(Dashboard.DISABLE_GRIPPER, Systems.gripper.disabled);
+		N.set(Dashboard.DISABLE_ROLLERS, Systems.rollerIntake.disabled);
+		N.set(Dashboard.DISABLE_CLAW, Systems.claw.disabled);
+		UPDATE_SYSTEM_DISABLE = false;
 	}
-    }
+
+	Systems.driveBase.voltage = N.get(Dashboard.PDP_VOLT, 0.0);
+	Systems.driveBase.current = N.get(Dashboard.PDP_CURR, 0.0);
+	Systems.driveBase.power = N.get(Dashboard.PDP_POWER, 0.0);
+	Systems.driveBase.temp = N.get(Dashboard.PDP_TEMP, 0.0);
+	Systems.elevator.current = N.get(Dashboard.ELEV_CURR, 0.0);
+	Systems.gripper.current = N.get(Dashboard.GRIPPER_CURR, 0.0);
+	Systems.elevator.position = N.get(Dashboard.ELEVATOR_POSITION, 0.0);
+
+	/*
+	 * STACK UPDATE
+	 */
+	var oldCount = Stack.totes;
+	var oldRC = Stack.hasRC;
+	Stack.totes = N.get(Dashboard.TOTE_COUNT, 0.0);
+	Stack.hasRC = N.get(Dashboard.HAS_RC, false);
+	if (oldRC !== Stack.hasRC) {
+		if (Stack.hasRC) {
+			Stack.addRC = true;
+		} else { // just released
+			Stack.clear = true;
+		}
+	}
+	if (oldCount !== Stack.totes) {
+		if (Stack.totes === 0) { // just released
+			Stack.clear = true;
+		} else {
+			Stack.addTote = Stack.totes - oldCount;
+		}
+	}
 }
 
 /**
  * Update the display
  */
 function update() {
-    var MIN_VOLTAGE = 11.0;
-    var MAX_VOLTAGE = 13;
-    var MAX_CURRENT = 160;
-    var MAX_POWER = 1400;
-    var MAX_TEMP = 80;
-    var MAX_E_CURRENT = 32;
-    var MAX_G_CURRENT = 36;
+	var MIN_VOLTAGE = 11.0;
+	var MAX_VOLTAGE = 13;
+	var VOLTAGE_WARNING_THRESHOLD = 10.0;
+	var MAX_CURRENT = 160;
+	var MAX_POWER = 1400;
+	var MAX_TEMP = 80;
+	var MAX_E_CURRENT = 32;
+	var MAX_G_CURRENT = 36;
 
-    /*
-     * SYSTEM STATUS
-     */
-    var elevPosition = Systems.elevator.position.toFixed(1);
+	/*
+	 * SYSTEM STATUS
+	 */
+	var elevPosition = Systems.elevator.position.toFixed(1);
 
-    var ON = "#2d4", OFF = "#d42";
-    $("#status_BASE").style.backgroundColor = Systems.driveBase.voltage < MIN_VOLTAGE ? OFF : ON;
-    $("#status_CLAW").style.backgroundColor = Systems.claw.disabled ? OFF : ON;
-    $("#status_ELEVATOR").style.backgroundColor = Systems.elevator.current > MAX_E_CURRENT || Systems.elevator.disabled ? OFF : ON;
-    $("#status_GRIPPER").style.backgroundColor = Systems.gripper.current > MAX_G_CURRENT || Systems.gripper.disabled ? OFF : ON;
-    $("#status_ROLLERS").style.backgroundColor = Systems.rollerIntake.disabled ? OFF : ON;
-    $("#status_BASE").innerHTML = Systems.driveBase.disabled ? "&times;" : "";
-    $("#status_CLAW").innerHTML = Systems.claw.disabled ? "&times;" : "";
-    $("#status_ELEVATOR").innerHTML = Systems.elevator.disabled ? "&times;" : elevPosition;
-    $("#status_GRIPPER").innerHTML = Systems.gripper.disabled ? "&times;" : "";
-    $("#status_ROLLERS").innerHTML = Systems.rollerIntake.disabled ? "&times;" : "";
+	var ON = "#2d4", OFF = "#d42";
+	$("#status_BASE").style.backgroundColor = Systems.driveBase.voltage < MIN_VOLTAGE ? OFF : ON;
+	$("#status_CLAW").style.backgroundColor = Systems.claw.disabled ? OFF : ON;
+	$("#status_ELEVATOR").style.backgroundColor = Systems.elevator.current > MAX_E_CURRENT || Systems.elevator.disabled ? OFF : ON;
+	$("#status_GRIPPER").style.backgroundColor = Systems.gripper.current > MAX_G_CURRENT || Systems.gripper.disabled ? OFF : ON;
+	$("#status_ROLLERS").style.backgroundColor = Systems.rollerIntake.disabled ? OFF : ON;
+	$("#status_BASE").innerHTML = Systems.driveBase.disabled ? "&times;" : "";
+	$("#status_CLAW").innerHTML = Systems.claw.disabled ? "&times;" : "";
+	$("#status_ELEVATOR").innerHTML = Systems.elevator.disabled ? "&times;" : elevPosition;
+	$("#status_GRIPPER").innerHTML = Systems.gripper.disabled ? "&times;" : "";
+	$("#status_ROLLERS").innerHTML = Systems.rollerIntake.disabled ? "&times;" : "";
 
-    $("#voltage").style.width = (Systems.driveBase.voltage / MAX_VOLTAGE) * 75 + 25 + "%";
-    $("#voltage").innerHTML = "Voltage &nbsp;&nbsp;&nbsp;&nbsp;" + Systems.driveBase.voltage.toFixed(1) + " V";
-    $("#voltage").style.backgroundColor = Systems.driveBase.voltage < MIN_VOLTAGE ? "#d74" : "#ccc";
-    $("#current").style.width = (Systems.driveBase.current / MAX_CURRENT) * 65 + 25 + "%";
-    $("#current").innerHTML = "Current &nbsp;&nbsp;&nbsp;&nbsp;" + Systems.driveBase.current.toFixed(0) + " A";
-    $("#current").style.backgroundColor = Systems.driveBase.current > MAX_CURRENT ? "#d74" : "#ccc";
-    $("#power").style.width = (Systems.driveBase.power / MAX_POWER) * 65 + 25 + "%";
-    $("#power").innerHTML = "Power &nbsp;&nbsp;&nbsp;&nbsp;" + Systems.driveBase.power.toFixed(0) + " W";
-    $("#power").style.backgroundColor = Systems.driveBase.power > MAX_POWER ? "#d74" : "#ccc";
-    $("#temp").style.width = (Systems.driveBase.temp / MAX_TEMP) * 65 + 25 + "%";
-    $("#temp").innerHTML = "Temperature &nbsp;&nbsp;&nbsp;&nbsp;" + Systems.driveBase.temp.toFixed(1) + " &deg;F";
-    $("#temp").style.backgroundColor = Systems.driveBase.temp > MAX_TEMP ? "#d74" : "#ccc";
+	$("#voltage").style.width = (Systems.driveBase.voltage / MAX_VOLTAGE) * 75 + 25 + "%";
+	$("#voltage").innerHTML = "Voltage &nbsp;&nbsp;&nbsp;&nbsp;" + Systems.driveBase.voltage.toFixed(1) + " V";
+	$("#voltage").style.backgroundColor = Systems.driveBase.voltage < MIN_VOLTAGE ? "#d74" : "#ccc";
+	$("#current").style.width = (Systems.driveBase.current / MAX_CURRENT) * 65 + 25 + "%";
+	$("#current").innerHTML = "Current &nbsp;&nbsp;&nbsp;&nbsp;" + Systems.driveBase.current.toFixed(0) + " A";
+	$("#current").style.backgroundColor = Systems.driveBase.current > MAX_CURRENT ? "#d74" : "#ccc";
+	$("#power").style.width = (Systems.driveBase.power / MAX_POWER) * 65 + 25 + "%";
+	$("#power").innerHTML = "Power &nbsp;&nbsp;&nbsp;&nbsp;" + Systems.driveBase.power.toFixed(0) + " W";
+	$("#power").style.backgroundColor = Systems.driveBase.power > MAX_POWER ? "#d74" : "#ccc";
+	$("#temp").style.width = (Systems.driveBase.temp / MAX_TEMP) * 65 + 25 + "%";
+	$("#temp").innerHTML = "Temperature &nbsp;&nbsp;&nbsp;&nbsp;" + Systems.driveBase.temp.toFixed(1) + " &deg;F";
+	$("#temp").style.backgroundColor = Systems.driveBase.temp > MAX_TEMP ? "#d74" : "#ccc";
 
-    /*
-     * STACK
-     */
-    if (Stack.clear) {
-	$(".stack").innerHTML = null;
-	Stack.clear = false;
-    }
-    if (Stack.addRC) {
-	var el = document.createElement("img");
-	el.src = "assets/rc.png";
-	el.className = "obj";
-	$(".stack").appendChild(el);
-	Stack.addRC = false;
-    }
-    if (Stack.addTote > 0) {
-	for (var i = 0; i < Stack.addTote; i++) {
-	    var el = document.createElement("img");
-	    el.src = "assets/tote.png";
-	    el.className = "obj";
-	    $(".stack").appendChild(el);
+	/*
+	 * STACK
+	 */
+	if (Stack.clear) {
+		$(".stack").innerHTML = null;
+		Stack.clear = false;
 	}
-	Stack.addTote = 0;
-    }
+	if (Stack.addRC) {
+		var el = document.createElement("img");
+		el.src = "assets/rc.png";
+		el.className = "obj";
+		$(".stack").appendChild(el);
+		Stack.addRC = false;
+	}
+	if (Stack.addTote > 0) {
+		for (var i = 0; i < Stack.addTote; i++) {
+			var el = document.createElement("img");
+			el.src = "assets/tote.png";
+			el.className = "obj";
+			$(".stack").appendChild(el);
+		}
+		Stack.addTote = 0;
+	}
 
-    $("#status_GRIPPER").style.bottom = Systems.elevator.position * 75 + 24 + "px";
-    $("#gripper").style.bottom = Systems.elevator.position * 75 + 56 + "px";
-    $(".stack").style.bottom = Systems.elevator.position * 75 + 20 + "px";
-    
-    /*
-     * ROBOT WARNING SYSTEM
-    */
-    if (!CONNECTED) return;
-    
-    
-    // call again
-    REQ_ANIM_FRAME_ID = requestAnimationFrame(update);
+	$("#status_GRIPPER").style.bottom = Systems.elevator.position * 75 + 24 + "px";
+	$("#gripper").style.bottom = Systems.elevator.position * 75 + 56 + "px";
+	$(".stack").style.bottom = Systems.elevator.position * 75 + 20 + "px";
+
+	/*
+	 * ROBOT WARNING SYSTEM
+	 */
+	if (!CONNECTED)
+		return;
+
+	if (Systems.driveBase.voltage < VOLTAGE_WARNING_THRESHOLD && Systems.driveBase.voltage > 0) // if it's 0 then chances are its a comms problem, not battery
+		RWS.alert(RWS.CAUTION, "Low Voltage", 10);
+	else if (Systems.driveBase.voltage > MIN_VOLTAGE)
+		RWS.reset("Low Voltage");
+	
+	if (Systems.driveBase.current > MAX_CURRENT)
+		RWS.alert(RWS.WARNING, "High Current Draw", 10);
+	else
+		RWS.reset("High Current Draw");
+
+	if (Systems.driveBase.temp > MAX_TEMP)
+		RWS.alert(RWS.CAUTION, "High Temp", 5);
+	else
+		RWS.reset("High Temp");
+
+	// call again
+	REQ_ANIM_FRAME_ID = requestAnimationFrame(update);
 }
 
 /**
  * one-time operations
- */ 
+ */
 function init() {
-    $("#build").innerHTML = "Running Y-OS version " + N.get(Dashboard.SOFTWARE_VERSION, 1).toFixed(1) + "." + BUILD_NUMBER;
+	$("#build").innerHTML = "Running Y-OS version " + N.get(Dashboard.SOFTWARE_VERSION, 1).toFixed(1) + "." + BUILD_NUMBER;
 }
+
+/**
+ * Create a caution or warning
+ * @param {string} message
+ */
+RWS = (function() {
+	var log = [];
+	var error = false;
+	var lastType = -1;
+	var lastPriority = -1;
+	var lastMessage = "";
+
+	var alert = function(type, message, priority) {
+		if (type <= lastType && priority <= lastPriority)
+			return;
+		if (error && message === lastMessage)
+			return;
+
+		lastType = type;
+		lastPriority = priority;
+		lastMessage = message;
+
+		$(".rws").innerHTML = message.toUpperCase();
+		$(".rws").style.backgroundColor = (type === 0) ? "#ec2" : "#e42";
+		$(".rws").style.display = "block";
+
+		log.push({
+			type: type,
+			message: message,
+			priority: priority,
+			time: new Date().toString()
+		});
+		error = true;
+
+		return this;
+	};
+
+	var clear = function() {
+		lastType = -1;
+		lastPriority = -1;
+		$(".rws").style.display = "none";
+	};
+
+	var reset = function(message) {
+		if (message === lastMessage && error) {
+			clear();
+			error = false;
+		}
+	};
+
+	return {
+		alert: alert,
+		clear: clear,
+		reset: reset,
+		log: function() {
+			console.log(log);
+		},
+		CAUTION: 0,
+		WARNING: 1
+	};
+})();
 
 /**
  * Testing
  */
 function test() {
-    N.set(Dashboard.PDP_CURR, 80);
-    N.set(Dashboard.PDP_POWER, 960);
-    N.set(Dashboard.PDP_TEMP, 78.7);
-    N.set(Dashboard.PDP_VOLT, 12.7);
-    N.set(Dashboard.ELEVATOR_POSITION, 1.5);
-    N.set(Dashboard.ELEV_CURR, 16);
-    N.set(Dashboard.GRIPPER_CURR, 24);
-    N.set(Dashboard.HAS_RC, false);
-    N.set(Dashboard.TOTE_COUNT, 3);
-    N.set(Dashboard.SOFTWARE_VERSION, 2.5);
-    UPDATE_AUTONOMOUS = true;
-    UPDATE_SYSTEM_DISABLE = true;
+	N.set(Dashboard.PDP_CURR, 80);
+	N.set(Dashboard.PDP_POWER, 960);
+	N.set(Dashboard.PDP_TEMP, 78.7);
+	N.set(Dashboard.PDP_VOLT, 12.7);
+	N.set(Dashboard.ELEVATOR_POSITION, 1.5);
+	N.set(Dashboard.ELEV_CURR, 16);
+	N.set(Dashboard.GRIPPER_CURR, 24);
+	N.set(Dashboard.HAS_RC, false);
+	N.set(Dashboard.TOTE_COUNT, 3);
+	N.set(Dashboard.SOFTWARE_VERSION, 2.5);
+	UPDATE_AUTONOMOUS = true;
+	UPDATE_SYSTEM_DISABLE = true;
 }
